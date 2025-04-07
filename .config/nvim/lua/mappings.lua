dfe16f04ab9a0c4caa4bc04364694fda6bff5137
Seth Miles <smiles@scalar.cloud>
2025-04-07 10:16:28 -0500

    ✨ Adds new mappings for general use
    Adds general, Copilot, and Telescope mappings for improved navigation and functionality.
    
    - Adds mapping to enter command mode with ';' key.
    - Adds mapping to exit insert mode with 'jk' sequence.
    - Adds mapping to accept copilot suggestions with Ctrl+l.
    - Adds mapping to find all files (including hidden) with <leader>ff.

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,13 +1,42 @@
 require "nvchad.mappings"
 
--- add yours here
+-- start below
 
 local map = vim.keymap.set
 
+-- general mappings
+
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-map('i', '<C-l>', function ()
-  vim.fn.feedkeys(vim.fn['copilot#Accept'](), '')
-end, { desc = 'Copilot Accept', noremap = true, silent = true })
+
+-- github copilot mappings
+
+-- <C-l> is used to accept the suggestion
+map(
+  'i',
+  '<C-l>',
+  function ()
+    vim.fn.feedkeys(vim.fn['copilot#Accept'](), '')
+  end,
+  {
+    desc = 'Copilot Accept',
+    noremap = true,
+    silent = true
+  }
+)
+
+
+-- telescope mappings
+
+-- find files looks through hidden files
+map(
+  'n',
+  '<leader>ff',
+  "<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>",
+  {
+    desc = 'Find ALL files',
+    noremap = true
+  }
+)
 
 -- map({ "n", "i", "v" }, "<sC-s>", "<cmd> w <cr>")
